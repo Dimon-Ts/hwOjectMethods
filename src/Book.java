38bc437e634088f887b51fea57a463ec11dfb8b6
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-10-14 18:12:28 +0300

    Методы объектов.Домашка:toString,equals,hashCode

---
+++ src/Book.java
@@ -0,0 +1,30 @@
+import java.util.Objects;
+
+public class Book {
+    private final String title;
+    private int  yearPublication;
+
+    public Book(String title, int  yearPublication) {
+        this.title = title;
+        this. yearPublication =  yearPublication;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public int getYearPublication() {
+        return yearPublication;
+    }
+    public void setYearPublication(int yearPublication ) {
+        this.yearPublication = yearPublication;
+    }
+    public String toString() {
+        return "Название книги -  " + this.title + " , дата издания - " + this.yearPublication + " ";
+    }
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+}
